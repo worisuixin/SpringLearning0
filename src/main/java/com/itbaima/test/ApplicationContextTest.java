088b54d8f316ca2320dd2f87356eb229d7d785ee
wori <worisuixin@163.com>
2025-02-01 22:09:21 +0800

    第二次提交，注释更新

--- src/main/java/com/itbaima/test/ApplicationContextTest.java
+++ src/main/java/com/itbaima/test/ApplicationContextTest.java
@@ -8,7 +8,7 @@ public class ApplicationContextTest {
     public static void main(String[] args) {
 //        这里这个url到底是谁，无所谓，只要是xml文件即可，只要里面配置了bean的信息就可以了。  不过一般是叫applicationContext.xml
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        UserService userService = (UserService) context.getBean("userService");
+        UserService userService = (UserService) context.getBean("userService");   //这里其实是会将那个id转换为这个beanname进行传递的
 //        这里就实现了几行代码实现了相同的功能，更加强大，所以用这个applicinationContext，但是底层还是BeanFactory
         System.out.println(userService);
     }
